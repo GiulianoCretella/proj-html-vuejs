d54579a95e9779bf79b31da712785df334e2a24f
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-18 18:07:56 +0200

    footer completo

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,31 @@
 <template>
-<section></section>
+<section class="row g-0">
+<footer-lists class="col-12 col-lg-6 list-side" :firstList="restaurantAddress" :secondList="timeTables"/>
+<div class="col-12 col-lg-6 img_container h-100">
+    <img class="img-fluid h-100" src="../assets/img/footer-img-1.jpg" alt="">
+</div>
+</section>
 </template>
 
 <script>
+import {restaurantAddress,timeTables} from '../dati.js'
+import FooterLists from './FooterLists.vue'
+
 export default {
-    name:'AppFooter'
+    name:'AppFooter',
+    components:{
+        FooterLists
+    },
+    data(){
+        return{
+            restaurantAddress:restaurantAddress,
+            timeTables:timeTables
+        }
+    }
 }
 </script>
-
-<style lang="scss">
-
+<style lang="scss" scoped>
+.list-side{
+    background-image: url(../assets/img/bj_jumbo.jpg);
+}
 </style>
---
+++ src/components/FooterLists.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="container">
+      <div class="row">
+          <ul class="col-4 p-0">
+              <li><span>find our restaurant</span></li>
+              <li  class="py-1" v-for="(item,index) in firstList" :key="index">
+                  <p>{{item.address}}</p>
+                  <p>{{item.city}}</p>
+                  <p>{{item.phoneNumber}}</p>
+              </li>
+          </ul>
+          <ul class="mx-2 col-4">
+              <li><span>Working Hours</span></li>
+              <li class="py-2" v-for="(item,index) in secondList" :key="index">
+                  <span>{{item.day}}</span>
+                  <p class="closed">{{item.openingTime}}</p>
+
+              </li>
+              <li>
+                  <span>follow us:</span>
+                  <span class="social mx-2">
+                      <i class="fa-brands fa-twitter"></i>
+                      <i class="fa-brands fa-facebook-f"></i>
+                      <i class="fa-brands fa-instagram"></i>
+                      <i class="fa-brands fa-linkedin-in"></i>
+                  </span>
+              </li>
+          </ul>
+          <div class="col">
+              <h6>the don peppe crew first an foremost valuse an authentic, well baked slice of pizza.</h6>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name:'FooterLists',
+    props:{
+        firstList:Array,
+        secondList:Array
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/vars.scss';
+section{
+    
+    padding: 50px;
+    span{
+    color:$main_gold;
+    text-transform:uppercase;
+    font-size:0.8em;
+    margin: 10px 0;
+    };
+    p{
+        color:$p_color;
+        font-size: 0.8em;
+        margin: 0
+    };
+    h6{
+        color: white;
+        text-transform: uppercase;
+    }
+    .fa-brands{
+        color: white;
+        margin: 0 5px;
+    }
+}
+
+</style>
